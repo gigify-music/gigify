440c7d7898a8c19eaaaee145448e04dcaeebbd26
JP Marra <jp.marra@me.com>
2017-03-31 19:59:15 -0400

    Event data received from songkick, being cleaned

--- server/router.js
+++ server/router.js
@@ -1,5 +1,8 @@
+const Controller = require('./controller.js');
 const express = require('express');
 
 const app = express.Router();
+
+app.get('/events', Controller.getEvents);
 
 module.exports = app;
