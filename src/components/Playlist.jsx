d7d4e9aba7681c1f3575cca6eebaafe1695cd2c1
Jordan Estes <jle48@georgetown.edu>
2017-04-05 18:29:58 -0400

    THIS

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -2,7 +2,15 @@ import React from 'react';
 
 
 const Playlist = () => (
-  <div>PLAYLIST BABY</div>
+  <div className="playlist-container">
+    <div className="playlist-view">
+      <h1>A preview of your new playlist:</h1>
+      <iframe
+        src="https://embed.spotify.com/?uri=spotify:user:spotify:playlist:3rgsDhGHZxZ9sB9DQWQfuf&theme=dark"
+        width="500" height="600" frameBorder="0" allowTransparency="true"
+      />
+    </div>
+  </div>
   );
 
 export default Playlist;
