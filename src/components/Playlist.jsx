362890ee55e2b0c8e7ec8293f87d05dc498cc9ca
JP Marra <jp.marra@me.com>
2017-04-06 16:57:39 -0400

    scrolling to playlist works

--- src/components/Playlist.jsx
+++ src/components/Playlist.jsx
@@ -1,15 +1,32 @@
 import React from 'react';
+import SweetScroll from 'sweet-scroll';
 
-const Playlist = ({ playlistId }) => (
-  <div className="playlist-container">
-    <div className="playlist-view">
-      <h1>A preview of your new playlist:</h1>
-      <iframe
-        src={`https://embed.spotify.com/?uri=spotify:user:${playlistId[0]}:playlist:${playlistId[1]}&theme=dark`}
-        width="500" height="600" frameBorder="0" allowTransparency="true"
-      />
-    </div>
-  </div>
-  );
-
+class Playlist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+  componentDidMount() {
+    this.sweetScroll = new SweetScroll();
+    console.log("COMPONENT MOUNTED");
+  }
+  componentDidUpdate() {
+    console.log("PLAYLIST DID UPDATE");
+    this.sweetScroll.toElement(document.getElementById('playlist-scroll'));
+  }
+  render() {
+    return (
+      <div className="playlist-container">
+        <div id="playlist-scroll" className="playlist-view">
+          <h1>A preview of your new playlist:</h1>
+          <iframe
+            src={`https://embed.spotify.com/?uri=spotify:user:${this.props.playlistId[0]}:playlist:${this.props.playlistId[1]}&theme=dark`}
+            width="500" height="600" frameBorder="0" allowTransparency="true"
+          />
+        </div>
+      </div>
+    )
+  }
+}
 export default Playlist;
