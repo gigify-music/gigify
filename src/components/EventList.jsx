459bd3cddac19fd6a14d05ac7518e1258ac88603
Jordan Estes <jle48@georgetown.edu>
2017-04-06 12:06:55 -0400

    Progress

--- src/components/EventList.jsx
+++ src/components/EventList.jsx
@@ -1,30 +1,45 @@
 import React, { PropTypes, Component } from 'react';
 import axios from 'axios';
+import ToggleDisplay from 'react-toggle-display';
 // import { connect } from 'react-redux';
 // import { toggleActive } from '../actions';
 import Event from './Event';
+
 
 class EventList extends Component {
   constructor(props) {
     super(props);
     this.state = {
       selected: {},
+      displayWarning: false,
     };
     this.toggleEvent = this.toggleEvent.bind(this);
     this.generatePlaylist = this.generatePlaylist.bind(this);
   }
 
   toggleEvent(performers, id) {
-    console.log('SELECTED ARTISTS', performers, id);
-
     // this.props.toggleActive(id);
 
+    console.log('SELECTED STATE', this.state.selected);
     const selected = this.state.selected;
+
     if (selected[id]) {
       delete selected[id];
+    }
+
+    const items = Object.values(this.state.selected);
+    const flatten = [].concat(...items);
+    const unique = [...new Set(flatten)];
+    console.log('NUMBER SELECTED', unique.length);
+
+    if (unique.length > 5) {
+      this.setState({
+        displayWarning: true,
+      });
     } else {
       selected[id] = performers;
       this.setState({
+        displayWarning: false,
         selected,
       });
     }
@@ -53,7 +68,14 @@ class EventList extends Component {
 
     return (
       <div id="event-page" className="event-page-container">
-        <button onClick={this.generatePlaylist}>Generate Playlist of Selected</button>
+        <div className="event-list-sidebar">
+          <button onClick={this.generatePlaylist}>Generate Playlist of Selected</button>
+          <ToggleDisplay show={this.state.displayWarning}>
+            <div className="selectionWarning">
+              HERE IS A WARNING
+            </div>
+          </ToggleDisplay>
+        </div>
         <ul className="list-group">
           <text>Selected performers:</text>
           {selectedPerformers.map(performer =>
@@ -61,6 +83,8 @@ class EventList extends Component {
               {performer}
             </li>)}
         </ul>
+
+
         <div className="event-list-container">
           <ul>
             {this.props.listings.map((event, i) =>
