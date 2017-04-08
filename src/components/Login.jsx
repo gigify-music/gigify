cbf2f2b6d4e634d40b7e38809ccc0782393803ac
Jordan Estes <jle48@georgetown.edu>
2017-04-07 23:31:49 -0400

    Particles working

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import requests from '../utils/requests';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import Particles from 'react-particles-js';
 
 class Login extends React.Component {
   handleAuth() {
@@ -8,18 +9,20 @@ class Login extends React.Component {
   }
   render() {
     return (
-      <div className="page-container">
-        <div className="login-container">
-          <Link to="/home">home</Link>
-          <label className="page-title"> Gigify </label>
-          <label className="page-subheader"> Create playlists based off of your
+
+      <div id="particles-js">
+        <div className="page-container">
+          <div className="login-container">
+            <label className="page-title"> Gigify </label>
+            <label className="page-subheader"> Create playlists based off of your
           upcoming songkick gigs </label>
-          <button
-            className="btn btn-lg spotify-auth-button" type="button"
-            onClick={() => this.handleAuth()}
-          >
-            <i className="fa fa-spotify fa-3x spotify-icon" aria-hidden="true" />
+            <button
+              className="btn btn-lg spotify-auth-button" type="button"
+              onClick={() => this.handleAuth()}
+            >
+              <i className="fa fa-spotify fa-3x spotify-icon" aria-hidden="true" />
            Connect with Spotify </button>
+          </div>
         </div>
       </div>
     );
