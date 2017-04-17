153a774327c47d5507789602b1a114c2ad7361b1
JP Marra <jp.marra@me.com>
2017-04-17 11:16:56 -0400

    fix(production): switching to dev deployment

--- server/controller.js
+++ server/controller.js
@@ -9,7 +9,7 @@ require('dotenv').config();
 const spotifyApi = new SpotifyWebApi({
   clientId: process.env.APP_KEY,
   clientSecret: process.env.APP_SECRET,
-  redirectUri: 'http://gigify.io/auth/callback',
+  redirectUri: 'http://localhost:8000/auth/callback',
 });
 
 const getArtistIDList = (artistList) => {
