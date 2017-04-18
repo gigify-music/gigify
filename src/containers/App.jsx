518ef5934de27fe5c13a1199fe1b29fe3881d0aa
iamongit <ayousuf2@binghamton.edu>
2017-04-18 18:57:58 -0400

    style(Linter errors fix): Fixed linter errors
    Fixed linter errors

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import Login from '.././components/Login';
-import Home from './Home';
 import '../../public/Styles/main.scss';
 
 class App extends Component {
--- src/containers/Genres.jsx
+++ src/containers/Genres.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types';
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import ToggleDisplay from 'react-toggle-display';
@@ -9,19 +10,17 @@ class Genres extends Component {
     super(props);
     this.state = {
       onLoadingGenreGif: false,
-    }
+    };
     this.handleGenre = this.handleGenre.bind(this);
   }
 
   handleGenre(username) {
-    this.setState({ onLoadingGenreGif: true })
-    const that = this;
-    that.props.gettingEvents();
+    this.setState({ onLoadingGenreGif: true });
+    this.props.gettingEvents();
     axios.get(`/api/events/${username}`)
     .then((response) => {
-      that.props.getEvents(response);
-      this.setState({ onLoadingGenreGif: false })
-      // that.setState({ showEventList: true });
+      this.props.getEvents(response);
+      this.setState({ onLoadingGenreGif: false });
     })
     .catch((error) => {
       console.log(error, 'error in get api/events on submit');
@@ -30,32 +29,34 @@ class Genres extends Component {
 
   render() {
     return (
-    <div className="genre-container">
-      <div className="dropdown">
-        <button
-          className="dropdown-btn btn btn-lg dropdown-toggle"
-          type="button" data-toggle="dropdown">
+      <div className="genre-container">
+        <div className="dropdown">
+          <button
+            className="dropdown-btn btn btn-lg dropdown-toggle"
+            type="button" data-toggle="dropdown"
+          >
           Choose a genre <i className="arrow fa fa-chevron-down" aria-hidden="true" />
-        </button>
-        <ul className="dropdown-menu genres">
-          <li><a onClick={() => this.handleGenre('gigify_edm')}><i className="fa fa-music" aria-hidden="true" /> EDM </a></li>
-          <li><a onClick={() => this.handleGenre('gigify_hiphop')}><i className="fa fa-music" aria-hidden="true" /> Hip Hop </a></li>
-          <li><a onClick={() => this.handleGenre('gigify_indie')}><i className="fa fa-music" aria-hidden="true" /> Indie </a></li>
-          <li><a onClick={() => this.handleGenre('gigify_pop')}><i className="fa fa-music" aria-hidden="true" /> Pop </a></li>
-          <li><a onClick={() => this.handleGenre('gigify_rock')}><i className="fa fa-music" aria-hidden="true" /> Rock </a></li>
-        </ul>
-      </div>
-      <div className="nyc-events">(NYC Gigs)</div>
-      <ToggleDisplay id="toggle-genre-gif" show={this.state.onLoadingGenreGif}>
-        <div className="input-loader">
-          <img
-            className="input-loader-gif" id="genre-loader"
-            src="../assets/loadingring.gif"
-          />
+          </button>
+          <ul className="dropdown-menu genres">
+            <li><a onClick={() => this.handleGenre('gigify_edm')}><i className="fa fa-music" aria-hidden="true" /> EDM </a></li>
+            <li><a onClick={() => this.handleGenre('gigify_hiphop')}><i className="fa fa-music" aria-hidden="true" /> Hip Hop </a></li>
+            <li><a onClick={() => this.handleGenre('gigify_indie')}><i className="fa fa-music" aria-hidden="true" /> Indie </a></li>
+            <li><a onClick={() => this.handleGenre('gigify_pop')}><i className="fa fa-music" aria-hidden="true" /> Pop </a></li>
+            <li><a onClick={() => this.handleGenre('gigify_rock')}><i className="fa fa-music" aria-hidden="true" /> Rock </a></li>
+          </ul>
         </div>
-      </ToggleDisplay>
-    </div>
-    )
+        <div className="nyc-events">(NYC Gigs)</div>
+        <ToggleDisplay id="toggle-genre-gif" show={this.state.onLoadingGenreGif}>
+          <div className="input-loader">
+            <img
+              className="input-loader-gif" id="genre-loader"
+              src="../assets/loadingring.gif"
+              alt="Smiley face"
+            />
+          </div>
+        </ToggleDisplay>
+      </div>
+    );
   }
 }
 
@@ -63,5 +64,10 @@ const mapStatetoProps = ({ events, loading }) => ({
   listings: events.eventListings,
   showLoadingGifGenre: loading,
 });
+
+Genres.propTypes = {
+  gettingEvents: PropTypes.func.isRequired,
+  getEvents: PropTypes.func.isRequired,
+};
 
 export default connect(mapStatetoProps, { getEvents, gettingEvents })(Genres);
