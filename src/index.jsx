ad77350675258d0bd5195e3eaf06fc63204ccd43
JP Marra <jp.marra@me.com>
2017-04-18 18:48:30 -0400

    refactor(index): refactored for style guide

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render } from 'react-dom';
+import thunk from 'redux-thunk';
 import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import reducer from './reducers';
-import thunk from 'redux-thunk';
 import Routes from './routes/Routes';
 
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
