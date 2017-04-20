ca9fce29c9c0a2f5dcb8e1cb6eddfaace638aae5
iamongit <ayousuf2@binghamton.edu>
2017-04-20 15:09:17 -0400

    scheduler add, tracker fix

--- server/cronjob.js
+++ server/cronjob.js
@@ -4,7 +4,7 @@ const sendNotification = require('./sendreminder');
 module.exports = {
   job: () => {
     const rule = new scheduler.RecurrenceRule();
-    rule.minute = 15;
+    rule.minute = 20;
     rule.hour = 15;
     scheduler.scheduleJob(rule, () => {
       sendNotification.sendNotification();
